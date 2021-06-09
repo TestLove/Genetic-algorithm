82c07cb81787c938fa8181291ceebc58067c6a10
testlove <1923161393@qq.com>
2021-06-09 10:02:51 +0800

    初始化项目框架

---
+++ src/main/java/cn/testlove/ga/Application.java
@@ -0,0 +1,11 @@
+package cn.testlove.ga;
+
+/**
+ * @author TestLove
+ * @version 1.0
+ * @date 2021/6/9 9:59
+ * @Description: null
+ */
+public class Application {
+
+}
---
+++ src/main/java/cn/testlove/ga/Gene.java
@@ -0,0 +1,10 @@
+package cn.testlove.ga;
+
+/**
+ * @author TestLove
+ * @version 1.0
+ * @date 2021/6/9 10:01
+ * @Description: null
+ */
+public class Gene {
+}
